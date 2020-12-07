c3d6f953be28e10e5780196f67a0aa388ab09f2a
LADENBURGER Pierre <ladenp@serwl.local>
2020-12-07 08:41:57 +0100

    contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Commun\GoogleMaps;
 use App\Entity\Contact;
 use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
+	private $entityManager;
+
+	function __construct(EntityManagerInterface $entityManager)
+	{
+		$this->googleMaps = new GoogleMaps();
+		$this->entityManager =  $entityManager;
+
+	}
+
     /**
      * @Route("/contact", name="contact")
      */
@@ -20,7 +31,11 @@ class ContactController extends AbstractController
         $formulaire = $this->createForm(ContactFormType::class, $contact);
         $formulaire->handleRequest($request);
         if ($formulaire->isSubmitted() && $formulaire->isValid()) {
-
+			$contact = $formulaire->getData();
+	        $this->entityManager->persist($contact);
+	        $this->entityManager->flush();
+	        $this->addFlash('success', 'Message envoyé');
+	        $this->redirectToRoute('contact');
         }
         return $this->render('contact/contact.html.twig', [
             'formulaire' => $formulaire->createView(),
